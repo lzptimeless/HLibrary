c57869bc0daff876d160203a730e4eb73db8b8d2
LingZhongping <zhongping.ling@razerzone.com>
2018-04-24 18:56:44 +0800

    Doing H.Book

--- code/HLibrarySln/H.Book/Metadata/HMetadataBookCover.cs
+++ code/HLibrarySln/H.Book/Metadata/HMetadataBookCover.cs
@@ -1,14 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace H.Book
 {
-    public class HMetadataBookCover : HMetadataBase
+    public class HMetadataBookCover : HMetadataSegment
     {
-        public UInt32 ThumbnailLength { get; set; }
-        public UInt32 ImageLength { get; set; }
+        public override byte ControlCode { get { return HMetadataControlCodes.BookCover; } }
+        public int ThumbnailLength { get; set; }
+        public int ImageLength { get; set; }
+
+        protected override int GetDataLength()
+        {
+            // 缩略图长度+图像长度
+            return 4 + 4;
+        }
+
+        protected override byte[] GetData()
+        {
+            if (ThumbnailLength < 0)
+                throw new ApplicationException($"Invalid ThumbnailLength: expected=[0,{int.MaxValue}], value={ThumbnailLength}");
+
+            if (ImageLength < 0)
+                throw new ApplicationException($"Invalid ImageLength: expected=[0,{int.MaxValue}], value={ImageLength}");
+
+            int dataLen = GetDataLength();
+            byte[] data = new byte[dataLen];
+            byte[] buffer;
+
+            // 写入缩略图长度
+            buffer = BitConverter.GetBytes(ThumbnailLength);
+            Array.Copy(buffer, 0, data, 0, 4);
+            // 写入图像长度
+            buffer = BitConverter.GetBytes(ImageLength);
+            Array.Copy(buffer, 0, data, 4, 4);
+
+            return data;
+        }
+
+        protected override void LoadData(byte[] data)
+        {
+            int expectedLength = GetDataLength();
+            if (data.Length != expectedLength)
+                throw new InvalidDataException($"data length error: expected={expectedLength}, value={data.Length}");
+
+            int thumbLen, imgLen;
+            // 读取缩略图长度
+            thumbLen = BitConverter.ToInt32(data, 0);
+            if (thumbLen < 0)
+                throw new InvalidDataException($"ThumbnailLength error: expected=[0,{int.MaxValue}], value={thumbLen}");
+
+            // 读取图像长度
+            imgLen = BitConverter.ToInt32(data, 4);
+            if (imgLen < 0)
+                throw new InvalidDataException($"ImageLength error: expected=[0,{int.MaxValue}], value={imgLen}");
+
+            ThumbnailLength = thumbLen;
+            ImageLength = imgLen;
+        }
     }
 }
--- code/HLibrarySln/H.Book/Metadata/HMetadataConstant.cs
+++ code/HLibrarySln/H.Book/Metadata/HMetadataConstant.cs
@@ -18,13 +18,5 @@ namespace H.Book
         /// The flag of control code
         /// </summary>
         public const byte ControlCodeFlag = 0xFF;
-        /// <summary>
-        /// 控制码总长度：<see cref="ControlCodeFlag"/> + 控制码
-        /// </summary>
-        public const int ControlCodeTotalLength = 2;
-        /// <summary>
-        /// 元数据段长度的长度
-        /// </summary>
-        public static readonly int MetadataSegmentLengthLength = System.Runtime.InteropServices.Marshal.SizeOf(typeof(long));
     }
 }
