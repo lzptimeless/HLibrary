76d7fa505c6cb8dd2efb7cd8d5c6b17a97dd546e
LingZhongping <lzptimeless@outlook.com>
2018-06-09 19:31:59 +0800

    同意颜色

--- code/HLibrarySln/Demo/MainWindow.xaml.cs
+++ code/HLibrarySln/Demo/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace Demo
             InitializeComponent();
 
             Loaded += MainWindow_Loaded;
-            Selection.ItemsSource = new[] { "Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10", "Item11", "Item12" };
+            Selection.ItemsSource = new[] { "Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10", "Item11", "Item12", "Item13", "Item14", "Item15", "Item16", "Item17", "Item18", "Item19", "Item20" };
             Selection.SelectedIndex = 2;
             //Selection.IsEnabled = false;
         }
