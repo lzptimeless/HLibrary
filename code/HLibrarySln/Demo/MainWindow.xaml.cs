155d06eaf77999724761a631dbbfb52e808b4afd
LingZhongping <lzptimeless@outlook.com>
2018-06-09 22:47:39 +0800

    调整颜色

--- code/HLibrarySln/Demo/MainWindow.xaml.cs
+++ code/HLibrarySln/Demo/MainWindow.xaml.cs
@@ -29,6 +29,9 @@ namespace Demo
             Selection.ItemsSource = new[] { "Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10", "Item11", "Item12", "Item13", "Item14", "Item15", "Item16", "Item17", "Item18", "Item19", "Item20" };
             Selection.SelectedIndex = 2;
             //Selection.IsEnabled = false;
+            Selection2.ItemsSource = new[] { "Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10", "Item11", "Item12", "Item13", "Item14", "Item15", "Item16", "Item17", "Item18", "Item19", "Item20" };
+            Selection2.SelectedIndex = 2;
+            List.ItemsSource = new[] { "Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10", "Item11", "Item12", "Item13", "Item14", "Item15", "Item16", "Item17", "Item18", "Item19", "Item20" };
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
