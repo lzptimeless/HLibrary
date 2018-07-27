172b005f086b8bca89cb565693462a0088cb5d09
LingZhongping <zhongping.ling@razer.com>
2018-07-27 18:43:07 +0800

    做HLibrary

--- code/HLibrarySln/H.BookLibrary/Services/DownloadService.cs
+++ code/HLibrarySln/H.BookLibrary/Services/DownloadService.cs
@@ -1,6 +1,7 @@
 ﻿using H.Book;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -12,11 +13,14 @@ namespace H.BookLibrary
     public class DownloadService
     {
         #region fields
-
+        private string _dir;
+        private DownloadCollection _items = new DownloadCollection();
         #endregion
 
         private DownloadService()
-        { }
+        {
+            _dir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "download");
+        }
 
         #region properties
         #region Instance
@@ -66,9 +70,19 @@ namespace H.BookLibrary
             throw new NotImplementedException();
         }
 
-        public Task DownloadAsync()
+        public Task DownloadAsync(string id)
         {
-            throw new NotImplementedException();
+            EnsureDirectoryExist();
+            string saveName = CreateSaveFileName(id);
+            string savePath = Path.Combine(_dir, saveName);
+            HitomiBookDownloader downloader = new HitomiBookDownloader(id, savePath);
+
+            DownloadItem item = new DownloadItem(id, downloader);
+            _items.Add(item);
+
+            downloader.
+
+            return downloader.DownloadAsync();
         }
 
         public Task StopAsync()
@@ -79,6 +93,77 @@ namespace H.BookLibrary
         public Task ResumeAsync()
         {
             throw new NotImplementedException();
+        }
+        #endregion
+
+        #region private methods
+        private void EnsureDirectoryExist()
+        {
+            if (!Directory.Exists(_dir)) Directory.CreateDirectory(_dir);
+        }
+
+        private static string CreateSaveFileName(string id)
+        {
+            return $"hitomi-{id}-{DateTime.Now.ToString("yyyy-MM-dd")}.hb";
+        }
+        #endregion
+
+        #region classes
+        private class DownloadItem
+        {
+            public DownloadItem(string id, HitomiBookDownloader downloader)
+            {
+                ID = id;
+                Downloader = downloader;
+            }
+
+            public string ID { get; private set; }
+            public HitomiBookDownloader Downloader { get; private set; }
+        }
+
+        private class DownloadCollection
+        {
+            #region fields
+            private OneManyLock _lock = new OneManyLock();
+            private List<DownloadItem> _items = new List<DownloadItem>();
+            #endregion
+
+            #region constructors
+
+            #endregion
+
+            #region properties
+
+            #endregion
+
+            #region public methods
+            public void Add(DownloadItem item)
+            {
+                _lock.Enter(true);
+                try
+                {
+                    if (FindIndex(item.ID) >= 0) throw new ArgumentException($"item already exist:id={item.ID}");
+
+                    _items.Add(item);
+                }
+                finally
+                {
+                    _lock.Leave();
+                }
+            }
+            #endregion
+
+            #region private methods
+            private int FindIndex(string id)
+            {
+                for (int i = 0; i < _items.Count; i++)
+                {
+                    if (_items[i].ID == id) return i;
+                }
+
+                return -1;
+            }
+            #endregion
         }
         #endregion
     }
